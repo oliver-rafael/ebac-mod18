448716708a0930cf0782a1a43e2cc1805f97374d
oliver-rafael <effael@gmail.com>
2024-03-17 18:55:31 +0000

    adicionado grafico, py e base em dev

--- gasolina.py
+++ gasolina.py
@@ -4,8 +4,8 @@ import seaborn as sns
 df_gasolina = pd.read_csv('./gasolina.csv')
 
 # grafico
-with sns.axes_style('darkgrid'):
-  grafico = sns.lineplot(data=df_gasolina, x='dia', y='venda', palette='dark')
-  grafico.set(title=' Valor médio da gasolina em São Paulo/SP - Julho de 2021', xlabel='Dez primeiros dias de Julho 2021', ylabel='Valor em R$')
-# Salva o gráfico como um arquivo PNG
+with sns.axes_style('whitegrid'):
+  grafico = sns.lineplot(data=df_gasolina, x='dia', y='venda', color ='red')
+  grafico.set(title=' Valor da gasolina em São Paulo - Julho de 2021', xlabel='Dez primeiros dias de Julho 2021', ylabel='Valor em R$')
+# Salvar o gráfico como um arquivo PNG
   grafico.figure.savefig('gasolina.png')
